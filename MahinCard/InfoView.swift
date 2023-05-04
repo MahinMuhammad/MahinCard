d28999891ecc601802b6e913280bb528b5fed49c
MahinMuhammad <amimahinurrahman@gmail.com>
2023-05-04 14:30:32 +0600

    mahin card

---
+++ MahinCard/InfoView.swift
@@ -0,0 +1,36 @@
+//
+//  InfoView.swift
+//  MahinCard
+//
+//  Created by Md. Mahinur Rahman on 5/4/23.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text:String
+    let imageView:String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .frame(height: 50)
+            .foregroundColor(Color.white)
+            .overlay(
+                HStack{
+                    Image(systemName: imageView)
+                        .imageScale(.large)
+                        .foregroundColor(Color.green)
+                    Text(text)
+                        .foregroundColor(.black)
+                }
+            )
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageView: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
---
+++ MahinCard/MahinCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MahinCardApp.swift
+//  MahinCard
+//
+//  Created by Md. Mahinur Rahman on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct MahinCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
